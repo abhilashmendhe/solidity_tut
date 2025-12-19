309f95658f1bfc35416a0a940fa682006b0fe6f3
abhilashmendhe <abhilashmendhe@gmail.com>
2025-12-19 12:50:12 +0530

    Modify: Added description about function visibility

--- 02_calculator.sol
+++ 02_calculator.sol
@@ -16,6 +16,22 @@ contract Calculator {
         result += a;
     }
 
+    /*
+        1. Visibility in functions
+            1.1 public - Functions with public visibility can be used internally and externally as well.
+            Your metamask account can use it. Other contracts can use it.
+
+            1.2 private - Can be used within contract only. Can be only used within code itself. You can't click
+            on it on the deployed contract sidebar(left) section. (Metamask acc can't use this.)
+
+            1.3 internal - Can be used within the contract and other inheriting contracts.
+
+            1.4 external - Can only be accessed from external contracts or accounts. Only used by external wallets of users.
+            It saves gas fees.
+            Can't call this func inside another func.
+            The difference is because in public functions, Solidity immediately copies array arguments to memory, while external functions can read directly from calldata. Memory allocation is expensive, whereas reading from calldata is cheap.
+            (https://ethereum.stackexchange.com/questions/19380/external-vs-public-best-practices)
+    */
     function sub(uint256 a) public {
         result -= a;
     }
