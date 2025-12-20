1b351df09754796a5ddd0d1b80e48ea2e61fd45e
abhilashmendhe <abhilashmendhe@gmail.com>
2025-12-20 16:04:46 +0530

    Add: Get all tweet likes from specific author

---
+++ 03_7_twitter.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: GPL-3.0
+import "@openzeppelin/contracts/utils/Strings.sol";
+pragma solidity >=0.8.2 <0.9.0;
+
+/*
+    Tasks
+    -----
+    11.1 Create a function, getTotalLikes, to get total Tweet Likes for the user
+        // USE parameters of author
+    11.2 Loop over all the tweets 
+    11.3 Sum up totalLikes 
+    11.4 Return totalLikes  
+*/
+
+
+contract Twitter {
+
+    uint256 public MX_TWEET_LEN = 280;
+    address public owner;
+    struct Tweet {
+        uint256 id;
+        address author;
+        string content;
+        uint256 timestamp;
+        // mapping (address => uint256) likes; 
+        uint256 likes;
+    }
+
+    mapping(address => Tweet[]) public tweets; // Like a KV pair
+
+    event TweetCreated(uint256 id, address indexed author, string content, uint256 timestamp);
+    event TweetLiked(address liker, address tweetAuthor, uint256 tweetId, uint256 likes);
+    event TweetUnliked(address unliker, address tweetAuthor, uint256 tweetId, uint256 likes);
+    
+    constructor() {
+        owner = msg.sender;
+
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner. Can't change the tweet length");
+        _;
+    }
+
+    function changeTweetLength(uint256 newTweetLen) public onlyOwner {
+        MX_TWEET_LEN = newTweetLen;
+    }
+
+    function createTweet(string memory content) public {
+
+        require(bytes(content).length <= MX_TWEET_LEN, string(abi.encodePacked("Tweet content length should be less than ", Strings.toString(MX_TWEET_LEN), " characters.")));
+
+        // remember,`memory` keyword tells to save the data in temp memory.
+        Tweet memory newTweet = Tweet({
+            id: uint256(tweets[msg.sender].length+1),
+            author: msg.sender,
+            content: content,
+            timestamp: block.timestamp,
+            likes: 0
+        });
+        tweets[msg.sender].push(newTweet);
+        emit TweetCreated(newTweet.id, newTweet.author, newTweet.content, newTweet.timestamp);
+    }
+
+    function getTweet(address _owner, uint256 id) public view returns (Tweet memory) {
+        require(tweets[_owner].length > 0, "No tweets are available.");
+        require(id > 0, "Tweet id should be greater than 0.");
+        require(id <= tweets[_owner].length, string(abi.encodePacked("Tweet id: ", Strings.toString(id), " not found.")));
+        return tweets[_owner][id-1];
+    }
+
+    function getAllTweet(address _owner) public view returns (Tweet[] memory) {
+        require(tweets[_owner].length > 0, "No tweets are available.");
+        return tweets[_owner];
+    }
+
+    function likeTweet(uint256 id, address author) external {
+        require(tweets[author].length > 0, "No tweets are available.");
+        require(id > 0, "Tweet id should be greater than 0.");
+        require(id <= tweets[author].length, string(abi.encodePacked("Tweet id: ", Strings.toString(id), " not found to like.")));
+        tweets[author][id-1].likes += 1;
+
+        emit TweetLiked(msg.sender, author, id, tweets[author][id-1].likes);
+    }
+    function unlikeTweet(uint256 id, address author) external {
+    require(tweets[author].length > 0, "No tweets are available.");
+        require(id > 0, "Tweet id should be greater than 0.");
+        require(id <= tweets[author].length, string(abi.encodePacked("Tweet id: ", Strings.toString(id), " not found to unlike.")));
+        tweets[author][id-1].likes -= 1;
+
+        emit TweetUnliked(msg.sender, author, id, tweets[author][id-1].likes);
+    }
+
+    function getTotalLikes(address author) public view returns (uint) {
+        
+        uint totalLikes = 0;
+
+        for (uint256 i = 0; i < tweets[author].length; i++) {
+            totalLikes += tweets[author][i].likes;
+        }
+        return totalLikes;
+    }
+}
